dcb785949abc91738ab75eab97163598e182758e
Shovickbarua <shovickbarua92@gmail.com>
2024-02-08 01:15:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
 function App() {
+  const {setUser} = useContext(UserContext);
   return (
     <div className="App">
       <header className="App-header">
